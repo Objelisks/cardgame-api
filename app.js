0dc932551db7b7e6f296a7af78b28e11a43aa0f9
Tim <objelisks@gmail.com>
2016-10-27 05:43:30 +0000

    cors

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ let express = require('express');
 let app = express();
 let port = 8082;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://cardgame-objelisks.c9users.io');
+    next();
+});
+
 // routers
 let card = require('./card/card.js');
-
 app.use('/cards', card);
 
 app.listen(port, () => {
